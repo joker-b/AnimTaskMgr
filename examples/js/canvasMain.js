31a5c8c5129c5ee423b00724320628a355eec012
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2015-06-28 01:05:34 -0700

    currently an infinite loop -- will add more!

--- examples/js/canvasMain.js
+++ examples/js/canvasMain.js
@@ -12,9 +12,6 @@ var ctx = cv.getContext('2d');
 ctx.fillStyle = 'rgb(200,0,0)';  
 ctx.fillRect(10, 10, 55, 50);
 function voila(TimeNow,RelativeTime,SinceLastFrameTime,SinceStartTime,Count) {
-	if (Count > 4500) {
-		return true;
-	}
 	var rc = "rgb("+((Count%50)*5)+",30,40)";
 	// console.log(rc);
 	ctx.fillStyle = "#ffff9f";
@@ -22,10 +19,10 @@ function voila(TimeNow,RelativeTime,SinceLastFrameTime,SinceStartTime,Count) {
 	ctx.fillStyle = rc;
 	var r = Count/40;
 	var nx = Math.random() * Math.random();
-	ctx.fillRect(200 + 180*Math.cos(r)-8*nx, 220 + 180*Math.sin(r)-8*nx, 50+16*nx, 50+16*nx);
+	ctx.fillRect(200 + 160*Math.cos(r)-8*nx, 180 + 160*Math.sin(r)-8*nx, 50+16*nx, 50+16*nx);
 }
 
-ATM.launch(voila);
+ATM.launch(voila,null,0.0);
 
 function animate() {
 	requestAnimationFrame(animate);
