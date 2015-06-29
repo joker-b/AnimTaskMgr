86cb882d1cd096579fe03856398bfac84380e2b1
Kevin Bjorke <kevin.bjorke+dev@gmail.com>
2015-06-29 02:36:02 -0700

    example refinements

--- examples/js/canvasMain.js
+++ examples/js/canvasMain.js
@@ -10,17 +10,19 @@ var cv = document.getElementById('cvMain');
 var ctx = cv.getContext('2d');
 
 
-function Orbitter(Ctx,cx,cy,A,BigR,InR,Speed,Alpha,Parent,KidLife) {
+function Orb(Ctx,cx,cy,A,BigR,InR,Speed,Alpha,Parent,KidLife,ParentAngle) {
 	this.ctx = Ctx;  // context
 	this.cx = cx;
 	this.cy = cy;
 	this.startAngle = A;
 	/* var hue = Math.floor(180*A/(2*Math.PI));
 	hue = (hue>360) ? (hue-360) : hue; */
-	var hue = Math.floor( 0.5 + Math.random() * 360);
-	var sat = (1 - BigR/200); // note constant
-	sat = Math.floor(100*Math.min(1,Math.max(0,sat)));
-	this.color = 'hsl('+hue+','+sat+'%,80%)';
+	var a = ParentAngle/(2*Math.PI);
+	a = a - Math.floor(a);
+	this.hue = Math.floor(a * 360);
+	this.sat = (1 - BigR/200); // note constant
+	this.sat = Math.floor(100*Math.min(1,Math.max(0,this.sat)));
+	this.color = 'hsl('+this.hue+','+this.sat+'%,70%)';
 	//console.log(this.color);
 	this.angle = 0.0;
 	this.speed = Speed;
@@ -32,13 +34,13 @@ function Orbitter(Ctx,cx,cy,A,BigR,InR,Speed,Alpha,Parent,KidLife) {
 	this.kidLife = KidLife;
 }
 
-Orbitter.prototype.setPos = function() {
+Orb.prototype.setPos = function() {
 	this.px = this.cx + this.bigR*Math.cos(this.angle);
 	this.py = this.cy + this.bigR*Math.sin(this.angle);
 };
 
-Orbitter.prototype.draw = function() {// graphics context
-	var nx = Math.random() * Math.random();
+Orb.prototype.draw = function() {// graphics context
+	var nx = 0.5; // Math.random() * Math.random();
 	this.ctx.globalAlpha  = this.alpha;
 	this.ctx.fillStyle = this.color;
 	this.ctx.beginPath();
@@ -46,6 +48,11 @@ Orbitter.prototype.draw = function() {// graphics context
 	this.ctx.closePath();
 	this.ctx.fill();
 	if (this.parent) {
+		var dx = this.px-this.parent.px;
+		var dy = this.py-this.parent.py;
+		var d = 8000.0 / (dx*dx+dy*dy);
+		d = (d>1.0) ? 1.0 : d;
+		this.ctx.globalAlpha  = d * this.alpha;
 		this.ctx.strokeStyle = this.color;
 		this.ctx.beginPath();
 		this.ctx.moveTo(this.px,this.py);
@@ -53,36 +60,26 @@ Orbitter.prototype.draw = function() {// graphics context
 		this.ctx.stroke();
 	}
 };
-Orbitter.prototype.animate = function(TimeNow,RelativeTime,SinceLastFrameTime,SinceStartTime,Count) {
-	this.angle = this.startAngle + this.speed*SinceStartTime;
-	//this.color = 'rgb('+((Count%50)*5)+',30,40)';
-	//this.setPos();
-	this.px = this.cx + this.bigR*Math.cos(this.angle);
-	this.py = this.cy + this.bigR*Math.sin(this.angle);
-	this.draw();
-	if (Math.random() < 0.6) {
-		this.spawn();
-	}
-};
-Orbitter.prototype.spawn = function() {
+Orb.prototype.spawn = function() {
 	var nbr = ((Math.random()*0.5)+0.5) * this.bigR * 0.75;
-	if (nbr<(this.rVar+1)) return; // too small
+	if (nbr<(this.rVar+1)) { return; }// too small
 	var d = Math.random() * this.kidLife + 500;
 	var ns = this.speed * this.bigR / nbr;
 	var na = Math.random() * 2 * Math.PI;
 
-	var nob = new Orbitter(this.ctx,
+	var nob = new Orb(this.ctx,
 							this.px+nbr*Math.cos(na),
 							this.py+nbr*Math.sin(na),
 							na+Math.PI, nbr,this.inR*0.5, -ns, this.alpha * 0.75,
-							this, this.kidLife * 0.7);
+							this, this.kidLife * 0.7, this.angle);
 	nob.launch(d);
 };
-Orbitter.prototype.launch = function(Duration) {
+Orb.prototype.launch = function(Duration) {
 	var orb = this;
 	function animate(TimeNow,RelativeTime,SinceLastFrameTime,SinceStartTime,Count) {
 		orb.angle = orb.startAngle + orb.speed*SinceStartTime;
-		// orb.color = 'rgb('+((255-Count%50))+',170,240)';
+		var L = Math.floor(90*(1 - RelativeTime));
+		orb.color = 'hsl('+orb.hue+','+orb.sat+'%,'+L+'%)';
 		orb.setPos();
 		orb.draw();
 		if (Math.random() < 0.025) {
@@ -92,13 +89,18 @@ Orbitter.prototype.launch = function(Duration) {
 	ATM.launch(animate,null,Duration);
 };
 
-var dad = new Orbitter(ctx,400,300, 0.0, 200,10, 0.0004, 1.0, null, 3500);
+var dad = new Orb(ctx,400,300, 0.0, 200,10, 0.0004, 1.0, null, 3500, 0);
 dad.launch(0); // dad is immortal
+
+var gAlpha = 0.05;
+ATM.launch(function animate(TimeNow,RelativeTime,SinceLastFrameTime,SinceStartTime,Count) {
+	gAlpha = 0.06+ 0.05*Math.sin(SinceStartTime*0.00026);
+},null,0);
 
 function animate() {
 	requestAnimationFrame(animate);
 	ctx.fillStyle = "#000";
-	ctx.globalAlpha = 0.06;
+	ctx.globalAlpha = gAlpha;
 	ctx.fillRect(0,0,800,600);
 	ATM.animate();
 }
